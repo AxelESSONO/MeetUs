da7c3999ed88e9a207a96235523935670393355f
Axel ESSONO <naxelcdric@gmail>
2020-08-06 08:49:12 +0200

    Working With FCM Token and Sign Out

--- app/src/main/java/com/obiangetfils/meetus/activities/MainActivity.java
+++ app/src/main/java/com/obiangetfils/meetus/activities/MainActivity.java
@@ -1,24 +1,114 @@
 package com.obiangetfils.meetus.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.obiangetfils.meetus.R;
+import com.obiangetfils.meetus.sign.SignInActivity;
+import com.obiangetfils.meetus.utilities.Constants;
+import com.obiangetfils.meetus.utilities.PreferenceManager;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PreferenceManager preferenceManager;
+    private TextView textTitle, textSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        textTitle = (TextView) findViewById(R.id.textTitle);
+        textSignOut = (TextView) findViewById(R.id.textSignOut);
 
+        textTitle.setText(String.format(
+                "%s %s",
+                preferenceManager.getString(Constants.KEY_FIRST_NAME),
+                preferenceManager.getString(Constants.KEY_LAST_NAME)
+        ));
 
+        textSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    sendFCMTokenToDatabase(task.getResult().getToken());
+                }
+            }
+        });
 
     }
+
+    private void sendFCMTokenToDatabase(String token) {
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTIONS_USERS).document(
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Token update successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Unable to send Token: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void signOut() {
+        Toast.makeText(this, "Sign Out ...", Toast.LENGTH_SHORT).show();
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTIONS_USERS).document(
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        preferenceManager.clearPreferences();
+                        startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Unable To Sign Out!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
 }

d43daf9deb69d6effdf8f041367c59f8f610b5c7
Axel ESSONO <naxelcdric@gmail>
2020-08-06 04:35:23 +0200

    Sign In Using Firestore and Shared Preferences

--- app/src/main/java/com/obiangetfils/meetus/sign/SignInActivity.java
+++ app/src/main/java/com/obiangetfils/meetus/sign/SignInActivity.java
@@ -2,14 +2,34 @@ package com.obiangetfils.meetus.sign;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.obiangetfils.meetus.R;
+import com.obiangetfils.meetus.activities.MainActivity;
+import com.obiangetfils.meetus.utilities.Constants;
+import com.obiangetfils.meetus.utilities.PreferenceManager;
 
 public class SignInActivity extends AppCompatActivity {
 
     private TextView textSignUp;
+    private EditText inputEmail, inputPassword;
+    private MaterialButton buttonSignin;
+    private ProgressBar signInProgressBar;
+    private PreferenceManager preferenceManager;
 
     String myToken = "eq9cVNQsQTyvipZ2Uu6dRr:APA91bGyWG3NCVrEXbpFWmgqlrjknhWhn2xvUMYvfEu-K9vSz9uRL9RIGxdTBW4dWBjLjMxrhVX2gHe3BeDVoBBzaQVGAlJ1dt2L22bj9R0-CBFoYGca4Lvcn_fJxh8XI0oS1n-OacAe";
 
@@ -18,6 +38,19 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        preferenceManager = new PreferenceManager(getApplicationContext());
+
+        if (preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        inputEmail = (EditText) findViewById(R.id.inputEmail);
+        inputPassword = (EditText) findViewById(R.id.inputPassword);
+        buttonSignin = (MaterialButton) findViewById(R.id.buttonSignIn);
+        signInProgressBar = (ProgressBar) findViewById(R.id.signInProgressBar);
+
         textSignUp = (TextView) findViewById(R.id.textSignUp);
         textSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +58,54 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
             }
         });
+
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputEmail.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(SignInActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(inputEmail.getText().toString()).matches()) {
+                    Toast.makeText(SignInActivity.this, "Enter valid Email", Toast.LENGTH_SHORT).show();
+                } else if (inputPassword.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(SignInActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                } else {
+                    signIn();
+                }
+            }
+        });
+
+    }
+
+    private void signIn() {
+        buttonSignin.setVisibility(View.INVISIBLE);
+        signInProgressBar.setVisibility(View.VISIBLE);
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTIONS_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0) {
+                            DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                            preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                            preferenceManager.putString(Constants.KEY_FIRST_NAME, documentSnapshot.getString(Constants.KEY_FIRST_NAME));
+
+                            preferenceManager.putString(Constants.KEY_LAST_NAME, documentSnapshot.getString(Constants.KEY_LAST_NAME));
+                            preferenceManager.putString(Constants.KEY_EMAIL, documentSnapshot.getString(Constants.KEY_EMAIL));
+
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        } else {
+                            signInProgressBar.setVisibility(View.INVISIBLE);
+                            buttonSignin.setVisibility(View.VISIBLE);
+                            Toast.makeText(SignInActivity.this, "Unable To Sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 }
--- app/src/main/java/com/obiangetfils/meetus/sign/SignUpActivity.java
+++ app/src/main/java/com/obiangetfils/meetus/sign/SignUpActivity.java
@@ -1,26 +1,81 @@
 package com.obiangetfils.meetus.sign;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.obiangetfils.meetus.R;
+import com.obiangetfils.meetus.activities.MainActivity;
+import com.obiangetfils.meetus.utilities.Constants;
+import com.obiangetfils.meetus.utilities.PreferenceManager;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private ImageView imageBack;
     private TextView textSignIn;
+    private EditText inputFirstName, inputLastName, inputEmail, inputPassword, inputConfirmPassword;
+    private MaterialButton buttonSignup;
+    private ProgressBar signupProgressBar;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up);
+        setContentView(R.layout.activity_sign_up);
 
+        preferenceManager = new PreferenceManager(getApplicationContext());
 
         imageBack = (ImageView) findViewById(R.id.imageBack);
         textSignIn = (TextView) findViewById(R.id.textSignIn);
+
+        inputFirstName = (EditText) findViewById(R.id.inputFirstname);
+        inputLastName = (EditText) findViewById(R.id.inputLastName);
+        inputEmail = (EditText) findViewById(R.id.inputEmail);
+        inputPassword = (EditText) findViewById(R.id.inputPassword);
+        inputConfirmPassword = (EditText) findViewById(R.id.inputConfirmPassword);
+        buttonSignup = (MaterialButton) findViewById(R.id.buttonSignUp);
+        signupProgressBar = (ProgressBar) findViewById(R.id.signupProgressBar);
+
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputFirstName.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Enter Fisrt Name", Toast.LENGTH_SHORT).show();
+                } else if (inputLastName.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Enter Last Name", Toast.LENGTH_SHORT).show();
+                } else if (inputEmail.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(inputEmail.getText().toString()).matches()) {
+                    Toast.makeText(SignUpActivity.this, "Enter valid Email", Toast.LENGTH_SHORT).show();
+                } else if (inputPassword.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                } else if (inputConfirmPassword.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Confirm Password", Toast.LENGTH_SHORT).show();
+                } else if (!inputPassword.getText().toString().equals(inputConfirmPassword.getText().toString())) {
+                    Toast.makeText(SignUpActivity.this, "Password and Confirm Password must be same!", Toast.LENGTH_SHORT).show();
+                } else {
+                    signup();
+                }
+
+            }
+        });
 
         textSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +88,43 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+    }
+
+    private void signup() {
+        buttonSignup.setVisibility(View.INVISIBLE);
+        signupProgressBar.setVisibility(View.VISIBLE);
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user = new HashMap<>();
+        user.put(Constants.KEY_FIRST_NAME, inputFirstName.getText().toString());
+        user.put(Constants.KEY_LAST_NAME, inputLastName.getText().toString());
+        user.put(Constants.KEY_EMAIL, inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD, inputPassword.getText().toString());
+
+        database.collection(Constants.KEY_COLLECTIONS_USERS)
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.KEY_FIRST_NAME, inputFirstName.getText().toString());
+                        preferenceManager.putString(Constants.KEY_LAST_NAME, inputLastName.getText().toString());
+                        preferenceManager.putString(Constants.KEY_EMAIL, inputEmail.getText().toString());
+
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                signupProgressBar.setVisibility(View.INVISIBLE);
+                buttonSignup.setVisibility(View.VISIBLE);
+                Toast.makeText(SignUpActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
